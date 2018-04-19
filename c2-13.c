42a2391320489bfcdb25050bc1b90fde224da374
蒋雨蒙 <jack.jiang.capu@gmail.com>
2018-04-19 18:20:12 +0800

    Update c2-13.c

--- c2-13.c
+++ c2-13.c
@@ -1,35 +1,23 @@
-/*
- * =====================================================================================
- *
- *       Filename:  c2-13.c
- *
- *    Description:  Determine if a positive integer is a prime number
- *
- *        Version:  1.0
- *        Created:  02/18/2018 13:16:13
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+
+int isPrime(int n) {
+	int i, flag = 1;
+	for (i = 2; i < sqrt(n); ++i) {
+		if (n % i == 0) {
+			flag = 0;
+			break;
+		}
+	}
+	return flag;
+}
+
 int main(void) {
     int n;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-    int i, flag = 1;
-    for (i = 0; i < n; ++i) {
-        if (n % i == 0) {
-            flag = 0;
-            break;
-        }
-    }
-    printf("%d\n", flag);
+    printf("%d\n", isPrime(n));
     return 0;
 }
 
